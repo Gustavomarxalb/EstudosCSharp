80c372e94b677254cd32f7530bb8109cc37bbbd2
Gustavo Marx <gustavoalbano01@gmail.com>
2026-08-24 21:17:14 -0300

    feat: adiciona o arquivo Program.cs e a classe Corretor

---
+++ Paradig/Corretor.cs
@@ -0,0 +1,114 @@
+﻿using System.IO;
+using System;
+
+namespace Paradig;
+
+public class Corretor
+{
+    public string[] Nomes { get; private set; }
+    public string[] CPFs { get; private set; }
+    public string[] Estados { get; private set; }
+    
+    private string[] Conteudo;
+
+
+    public Corretor()
+    {
+        LerDados();
+        Nomes = ConsertaNomes();
+        Estados = SiglaEstado();
+        CPFs = ConsertaCpf();
+    }
+    
+    public void LerDados()
+    {
+        Conteudo = File.ReadAllLines("tema11.txt");
+    }
+
+    public string[] ConsertaNomes()
+    {
+        string[] nomes = new string[Conteudo.Length];
+
+        for (int i = 0; i < Conteudo.Length; i++)
+        {
+            string coluna = Conteudo[i].Split('|')[0];
+            coluna = coluna.TrimEnd();
+
+            while (coluna.Contains("  "))
+            {
+                coluna = coluna.Replace("  ", " ");
+            }
+
+            if (coluna.Contains("nome"))
+                coluna = coluna.Remove(coluna.IndexOf("nome"));
+            nomes[i] = coluna;
+        }
+
+        return nomes;
+    }
+
+    public string[] SiglaEstado()
+    {
+        string[] estados = new string[Conteudo.Length];
+
+        for (int i = 0; i < Conteudo.Length; i++)
+        {
+            string coluna = Conteudo[i].Split('|')[2].Trim().ToUpper();
+            
+            if (coluna.Length == 2 && !coluna.Contains(" "))
+            {
+                estados[i] = coluna;
+                continue;
+            }
+            
+            var partes = coluna.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (partes.Length == 1)
+            {
+                estados[i] = partes[0].Substring(0, 2);
+            }
+            else
+            {
+                estados[i] = $"{partes[0][0]}{partes[^1][0]}";
+            }
+            
+        }
+        if (estados[0] == "ES")
+            estados[0] = estados[0].Remove(estados[0].IndexOf("ES")); 
+        return estados;
+    }
+
+    public string[] ConsertaCpf()
+    {
+        string[] cpf = new string[Conteudo.Length];
+
+        for (int i = 0; i < Conteudo.Length; i++)
+        {
+            string coluna = Conteudo[i].Split('|')[1].Trim();
+            
+            if (coluna.Contains("cpf"))
+            {
+                cpf[i] = "";
+                continue;
+            }
+            
+            string apenasNumeros = System.Text.RegularExpressions.Regex.Replace(coluna, @"\D", "");
+            
+            if (apenasNumeros.Length == 11)
+            {
+                
+                string bloco1 = apenasNumeros.Substring(0, 3); 
+                string bloco2 = apenasNumeros.Substring(3, 3); 
+                string bloco3 = apenasNumeros.Substring(6, 3); 
+                string digitos = apenasNumeros.Substring(9, 2); 
+                
+                cpf[i] = $"{bloco1}.{bloco2}.{bloco3}-{digitos}";
+            }
+            else
+            {
+                cpf[i] = apenasNumeros;
+            }
+        }
+        return cpf;
+    }
+}
---
+++ Paradig/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Paradig;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var obj = new Corretor();
+
+        string[] nomes = obj.Nomes;
+        string[] estados = obj.Estados;
+        string[] CPFs = obj.CPFs;
+        for (int i = 0; i < obj.Nomes.Length; i++)
+        {
+            if (i != 0)
+            {
+                Console.WriteLine(nomes[i] + " | " + CPFs[i] + " | " + estados[i]);
+            }
+            else
+            {
+                Console.WriteLine("Nome" + " | " + "CPF" + " | " + "Estado");
+            }
+        }
+    }
+}
